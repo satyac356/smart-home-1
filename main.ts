f81310c138f937f11d93379df90d827708799336
satyac356 <chintasatya360@gmail.com>
2026-05-01 23:43:05 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let noise = 0
+let light2 = 0
+led.enable(false)
+let strip = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
+basic.forever(function () {
+    light2 = smarthome.ReadLightIntensity(AnalogPin.P3)
+    if (light2 < 50) {
+        noise = smarthome.ReadNoise(AnalogPin.P2)
+        if (noise > 70) {
+            strip.showColor(neopixel.colors(NeoPixelColors.White))
+            basic.pause(10000)
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        }
+    }
+})
